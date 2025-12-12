430eb543b71d271e7793f70f4283b83db9e61a2a
Dinesh-Johnson <dinesh.xworkz@gmail.com>
2025-12-12 09:02:34 +0530

    console logs

---
+++ self-introduction.js
@@ -0,0 +1,8 @@
+console.log("Hello everyone!");
+console.log("My name is Dinesh Johnson.");
+console.log("I am currently doing an internship at X-workz.");
+console.log("I have experience as a full stack developer, including building dynamic web applications.");
+console.log("I also have hands-on experience working with both frontend and backend technologies.");
+console.log("In my free time, I love playing PC games.");
+console.log("I enjoy watching movies and web series.");
+console.log("Thank you!");
